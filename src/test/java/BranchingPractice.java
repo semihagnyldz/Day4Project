87f62ef28d22030e9dd230e0954fba18016389f5
semihagnyldz <semihagn@gmail.com>
2021-06-01 21:53:10 -0400

    things we have done so far!

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,10 @@
 public class BranchingPractice {
     public static void main(String[] args) {
+// created new project
+        //enabled version control
+        //added .gitignore with .idea folder, target, iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
 
     }
 }
